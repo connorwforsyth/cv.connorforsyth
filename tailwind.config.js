097c5fca151fd866b7848e409ca9b21baa9fc092
Connor Forsyth <connor@connorforsyth.co>
2024-08-29 11:23:59 +1000

    adding post css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js,ts,jsx,tsx}", "main.css"],
+  theme: {
+    fontFamily: {
+      sans: ["Geist", "sans-serif"],
+      mono: ["Geist Mono", "monospace"],
+    },
+  },
+  plugins: [],
+};
